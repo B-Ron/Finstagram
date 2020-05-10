8316068f961e6d48922266bbf6e69212b31209b0
Elwin <readyelwin@gmail.com>
2020-05-09 23:51:34 -0400

    Initial Commit

---
+++ Finstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finstagram
+//
+//  Created by user167669 on 5/9/20.
+//  Copyright © 2020 user167669. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
